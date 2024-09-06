5c47bc112f7e38413ff1a3e4bd49d100abbf7a22
Jayjolie1026 <74806676+Jayjolie1026@users.noreply.github.com>
2024-09-05 23:33:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const sql = require('mssql');
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 3000;
 
 const dbConfig = {
     user: 'BakeryAdmin',
