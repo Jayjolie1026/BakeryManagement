ad54e5bd75485c348d5844b3a15bffdddcff8477
Jamie Boyd <jayjolie1026@gmail.com>
2024-09-06 15:28:46 -0500

    l

--- server.js
+++ server.js
@@ -319,7 +319,7 @@ app.post('/login', async (req, res) => {
         const result = await request
             .input('username', sql.VarChar, username)
             .input('password', sql.VarChar, password)
-            .query('SELECT COUNT(*) from Users WHERE "username" = @username AND "password_hash" = @password');
+            .query('SELECT password_hash from Users WHERE "username" = @username');
         
         console.log('testing');
         if (result.recordset.length > 0) {
