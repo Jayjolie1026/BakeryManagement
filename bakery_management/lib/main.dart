b81eb17a6d73d79db28e577ee11f2a1b92814b40
Jamie Boyd <jayjolie1026@gmail.com>
2024-09-13 19:19:49 -0500

    working create account

--- bakery_management/lib/main.dart
+++ bakery_management/lib/main.dart
@@ -311,7 +311,10 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
     print("Response body: ${response.body}");
 
     if (response.statusCode == 201) {
-      Navigator.pop(context); // Go back to the previous page (sign-in page)
+       Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const BakeryHomePage()),
+      );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to create account: ${response.body}')),
