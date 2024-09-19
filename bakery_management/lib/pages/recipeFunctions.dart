c852a56eb846c77dd910f5b04a48c3dac108b9bd
layoung43 <layoung43.ttu@gmail.com>
2024-09-19 11:56:08 -0500

    Created recipeDetailPage and recipeUpdatePage

--- bakery_management/lib/pages/recipeFunctions.dart
+++ bakery_management/lib/pages/recipeFunctions.dart
@@ -207,88 +207,24 @@ class _AddRecipePageState extends State<AddRecipePage> {
   }
 }
 
-
 class DetailedRecipePage extends StatelessWidget {
   final String recipeName;
-  final int recipeID;
+  final String searchQuery = "";
+  final int recipeID; 
 
-  const DetailedRecipePage({required this.recipeName, required this.recipeID, Key? key}) : super(key: key);
+  DetailedRecipePage({required this.recipeName, required this.recipeID, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bagels Recipe and Directions'),
-      ),
-      body: SingleChildScrollView( // Use SingleChildScrollView to handle scrolling if content is long
-        padding: const EdgeInsets.all(16.0), // Add padding to the body
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // Align content to the left
-          children: [
-            // Image section
-            Image.asset(
-              'assets/bagel3.jpg', // Replace with your image path
-              width: double.infinity, // Make the image take up full width
-              height: 200, // Set a fixed height for the image
-              fit: BoxFit.cover, // Make the image cover the available space
-            ),
-            const SizedBox(height: 20), // Space between the image and the headers
-
-            // Ingredients header
-            const Text(
-              'Ingredients',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10), // Space between the header and content
-
-            // Placeholder for ingredients (You can populate this later with API data)
-            const Text(
-              'Loading ingredients...', // Placeholder text
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20), // Space between ingredients and directions
-
-            // Directions header
-            const Text(
-              'Directions',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10), // Space between the header and content
-
-            // Placeholder for directions (You can populate this later with API data)
-            const Text(
-              'Loading directions...', // Placeholder text
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
+        toolbarHeight: 100,
+        title: Image.asset(
+          'assets/recipe2.png',
+          height: 100,
         ),
-      ),
-    );
-  }
-}
-
-// Create a widget to display the data
-class RecipeListPage extends StatelessWidget {
-  final String searchQuery;
-
-  RecipeListPage({required this.searchQuery});
-  /*
-  final String recipeName;
-  final int recipeID;
-
-  const RecipeListPage({required this.recipeName, required this.recipeID, Key? key}) : super(key: key);
-  */
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recipe List'),
+        centerTitle: true,
+        backgroundColor: const Color(0xEEC07B),
       ),
       body: FutureBuilder<List<Item>>(
         future: RecipeApi.getItems(searchQuery),
@@ -299,61 +235,102 @@ class RecipeListPage extends StatelessWidget {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (snapshot.hasData) {
             final items = snapshot.data!;
+            final recipe = items.where((item) => item.recipeID == recipeID).toList();
 
-            return ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
+            if (recipe.isEmpty) {
+              return const Center(child: Text('Recipe not found'));
+            }
 
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
-                    ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          title: Text(item.name, style: TextStyle(fontWeight: FontWeight.bold)),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Product ID: ${item.productID}'),
-                              Text('Steps: ${item.steps}'),
-                            ],
-                          ),
+            final item = recipe.first;
+
+            return Stack(
+              children: [
+                SingleChildScrollView(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Recipe title
+                      Text(
+                        "$recipeName Recipe",
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Divider(), // Adds a divider line between sections
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Ingredients:',
-                                style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      
+                      // Recipe image
+                      Image.asset(
+                        'assets/bagel3.jpg', // Replace with your image path
+                        width: double.infinity,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(height: 20),
+                      
+                      // Ingredients section
+                      const Text(
+                        'Ingredients',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      
+                      if (item.ingredients.isNotEmpty)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: item.ingredients.map<Widget>((ingredient) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                '- ${ingredient.quantity} of ${ingredient.name}',
+                                style: const TextStyle(fontSize: 18),
                               ),
-                              const SizedBox(height: 8),
-                              ...item.ingredients.map((ingredient) {
-                                return ListTile(
-                                  contentPadding: EdgeInsets.zero,
-                                  title: Text(ingredient.name),
-                                  subtitle: Text('Quantity: ${ingredient.quantity}g'),
-                                );
-                              }).toList(),
-                            ],
-                          ),
+                            );
+                          }).toList(),
+                        )
+                      else
+                        const Text(
+                          'No ingredients available',
+                          style: TextStyle(fontSize: 18),
                         ),
-                      ],
-                    ),
+                      
+                      const SizedBox(height: 20),
+                      
+                      // Directions section
+                      const Text(
+                        'Directions',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        item.steps.replaceAll('\\n', '\n'),
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      const SizedBox(height: 80), // Add space to prevent text overlap with FAB
+                    ],
                   ),
-                );
-              },
+                ),
+                Positioned(
+                  bottom: 20, // Position the button above the text
+                  right: 20,
+                  child: FloatingActionButton.extended(
+                    onPressed: () async {
+                      // Open the recipe update dialog
+                      await showRecipeUpdateDialog(
+                        context,
+                        item, // Pass the current recipe item
+                        (updatedRecipe) {
+                          // Handle the updated recipe
+                          // Since we're in a StatelessWidget, this should be updated in state management
+                        },
+                      );
+                    },
+                    label: const Text('Update Recipe'),
+                    backgroundColor: const Color(0xFFF5E0C3),
+                  ),
+                ),
+              ],
             );
           } else {
             return const Center(child: Text('No data found'));
@@ -363,3 +340,206 @@ class RecipeListPage extends StatelessWidget {
     );
   }
 }
+
+
+Future<void> showRecipeUpdateDialog(BuildContext context, Item recipe, ValueChanged<Item> onRecipeUpdated) async {
+  TextEditingController _nameController = TextEditingController(text: recipe.name);
+  TextEditingController _stepsController = TextEditingController(text: recipe.steps);
+  TextEditingController _ingredientsController = TextEditingController(text: recipe.ingredients.join(", "));
+
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Update Recipe'),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Editable Fields for Recipe
+              TextField(
+                controller: _nameController,
+                style: const TextStyle(color: Color(0xFF6D3200)),
+                decoration: const InputDecoration(
+                  labelText: 'Name', 
+                  labelStyle: TextStyle(color: Color(0xFF6D3200)),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                ),
+              ),
+              TextField(
+                controller: _ingredientsController,
+                style: const TextStyle(color: Color(0xFF6D3200)),
+                decoration: const InputDecoration(
+                  labelText: 'Ingredients (comma-separated)', 
+                  labelStyle: TextStyle(color: Color(0xFF6D3200)),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                ),
+              ),
+              TextField(
+                controller: _stepsController,
+                style: const TextStyle(color: Color(0xFF6D3200)),
+                decoration: const InputDecoration(
+                  labelText: 'Steps', 
+                  labelStyle: TextStyle(color: Color(0xFF6D3200)),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF6D3200)),
+                  ),
+                ),
+                maxLines: 5, // Allow multiline for recipe steps
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Close dialog without changes
+            },
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Update the recipe with new values
+              final updatedRecipe = Item(
+                recipeID: recipe.recipeID, // Keep the same recipe ID
+                name: _nameController.text,
+                steps: _stepsController.text,
+                productID: recipe.productID,
+                ingredients: _ingredientsController.text.split(',').map((ingredientString) {
+                  // Split each string by ':' to get ingredientID, name, and quantity
+                final parts = ingredientString.split(':');
+
+                if (parts.length == 3) {
+                  return Ingredient(
+                    ingredientID: int.parse(parts[0].trim()), // Convert ingredientID to int
+                    name: parts[1].trim(),
+                    quantity: int.parse(parts[2].trim()), // Assuming quantity is a double
+                  );
+                } else {
+                  throw Exception('Invalid ingredient format');
+                }
+                }).toList(),  // Convert back to a list
+                            );
+
+              // Call the update callback
+              onRecipeUpdated(updatedRecipe);
+
+              // Close the dialog
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF6D3200),
+              foregroundColor: const Color(0xFFF0d1a0),
+            ),
+            child: const Text('Update'),
+          ),
+        ],
+      );
+    }
+  );
+}
+
+
+// // Create a widget to display the data
+// class RecipeListPage extends StatelessWidget {
+//   final String searchQuery;
+
+//   RecipeListPage({required this.searchQuery});
+//   /*
+//   final String recipeName;
+//   final int recipeID;
+
+//   const RecipeListPage({required this.recipeName, required this.recipeID, Key? key}) : super(key: key);
+//   */
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Recipe List'),
+//       ),
+//       body: FutureBuilder<List<Item>>(
+//         future: RecipeApi.getItems(searchQuery),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return const Center(child: CircularProgressIndicator());
+//           } else if (snapshot.hasError) {
+//             return Center(child: Text('Error: ${snapshot.error}'));
+//           } else if (snapshot.hasData) {
+//             final items = snapshot.data!;
+
+//             return ListView.builder(
+//               itemCount: items.length,
+//               itemBuilder: (context, index) {
+//                 final item = items[index];
+
+//                 return Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+//                   child: Container(
+//                     decoration: BoxDecoration(
+//                       border: Border.all(color: Colors.grey.shade300),
+//                       borderRadius: BorderRadius.circular(8),
+//                       color: Colors.white,
+//                     ),
+//                     child: Column(
+//                       children: [
+//                         ListTile(
+//                           title: Text(item.name, style: TextStyle(fontWeight: FontWeight.bold)),
+//                           subtitle: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Text('Product ID: ${item.productID}'),
+//                               Text('Steps: ${item.steps}'),
+//                             ],
+//                           ),
+//                         ),
+//                         const Padding(
+//                           padding: const EdgeInsets.symmetric(horizontal: 16),
+//                           child: Divider(), // Adds a divider line between sections
+//                         ),
+//                         Padding(
+//                           padding: const EdgeInsets.all(16),
+//                           child: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               const Text(
+//                                 'Ingredients:',
+//                                 style: TextStyle(fontWeight: FontWeight.bold),
+//                               ),
+//                               const SizedBox(height: 8),
+//                               ...item.ingredients.map((ingredient) {
+//                                 return ListTile(
+//                                   contentPadding: EdgeInsets.zero,
+//                                   title: Text(ingredient.name),
+//                                   subtitle: Text('Quantity: ${ingredient.quantity}g'),
+//                                 );
+//                               }).toList(),
+//                             ],
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 );
+//               },
+//             );
+//           } else {
+//             return const Center(child: Text('No data found'));
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
